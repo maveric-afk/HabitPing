1bb3a9318c76b53a0b186a3c13c0cc89afb71af1
Chirag Gupta <guptachirag965@gmail.com>
2025-12-09 15:09:22 +0530

    commit-11

--- Backend/index.js
+++ Backend/index.js
@@ -28,7 +28,11 @@ app.use(cookieParser())
 
 mongoose.connect(process.env.MONGO_URI)
 .then((res)=>{
-    console.log(`MongoDB connected`)
+    console.log(`MongoDB connected`);
+    const timer=setInterval(() => {
+    worker1()
+}, 60*1000);
+
 })
 .catch((e)=>{
     console.log(e);
@@ -36,10 +40,6 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use('/api/user',userRouter);
 app.use('/api/task',LoggedinOnly,taskRouter);
-
-const timer=setInterval(() => {
-    worker1()
-}, 60*1000);
 
 app.listen(PORT,()=>{
     console.log(`Server started at PORT ${PORT}`);
