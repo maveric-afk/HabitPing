ff6ec8e610ddc4793b769f0fdfa4bd87bc393d89
Chirag Gupta <guptachirag965@gmail.com>
2025-12-09 19:03:29 +0530

    commit-15

--- Backend/controllers/reminder.js
+++ Backend/controllers/reminder.js
@@ -7,9 +7,20 @@ const { getMessaging } = require('firebase-admin/messaging');
 const dotenv= require('dotenv');
 
 dotenv.config();
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_ADMIN.replace(/\\n/g, "\n")
-);
+
+const serviceAccount = {
+    type:process.env.FIREBASE_TYPE,
+    project_id:process.env.FIREBASE_PROJECT_ID,
+    private_key_id:process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key:process.env.FIREBASE_PRIVATE_KEY,
+    client_email:process.env.FIREBASE_CLIENT_EMAIL,
+    client_id:process.env.FIREBASE_CLIENT_ID,
+    auth_uri:process.env.FIREBASE_AUTH_URI,
+    token_uri:process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url:process.env.FIREBASE_AUTH_PROVIDER,
+    client_x509_cert_url:process.env.FIREBASE_CLIENT_URL,
+    universe_domain:process.env.FIREBASE_UNIVERSE_DOMAIN
+}
 
 
 
